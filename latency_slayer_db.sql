c76cee3745d75ddc89546bf8ce5b6bad474f6e44
BryanRO-SpTech <bryan.grocha@sptech.school>
2025-04-05 21:50:50 -0300

    Script SQL update

--- latency_slayer_db.sql
+++ latency_slayer_db.sql
@@ -1,20 +1,22 @@
-CREATE DATABASE latency_slayer;
+DROP DATABASE IF EXISTS latency_slayer;
+CREATE DATABASE IF NOT EXISTS latency_slayer;
 USE latency_slayer;
 
 CREATE TABLE country (
 	id_country INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(45) NOT NULL,
-	country_code CHAR(2) NOT NULL,
+    name VARCHAR(45) NOT NULL UNIQUE,
+	country_code CHAR(2) NOT NULL UNIQUE,
     mask_company_registration_number VARCHAR(45) NOT NULL,
     mask_phone VARCHAR(45) NOT NULL
 );
     
 CREATE TABLE contact (
 	id_contact INT PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL,
-    phone VARCHAR(16) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    phone VARCHAR(16) NOT NULL UNIQUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL);
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL
+);
     
 CREATE TABLE company (
 	id_company INT PRIMARY KEY AUTO_INCREMENT,
@@ -23,13 +25,17 @@ CREATE TABLE company (
     registration_number VARCHAR(16) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
-    fk_contact INT NOT NULL,
-    fk_country INT NOT NULL);
+    fk_country INT NOT NULL,
+    fk_contact INT NOT NULL UNIQUE,
+    CONSTRAINT fk_commpany_contact FOREIGN KEY (fk_contact) REFERENCES contact (id_contact),
+    CONSTRAINT fk_company_country FOREIGN KEY (fk_country) REFERENCES country (id_country)
+);
     
 CREATE TABLE opt_role (
 	id_opt_role INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(45) NOT NULL,
-    description VARCHAR(80));
+    name VARCHAR(45) NOT NULL UNIQUE,
+    description VARCHAR(80)
+);
     
 CREATE TABLE employee (
 	id_employee INT PRIMARY KEY AUTO_INCREMENT,
@@ -39,49 +45,53 @@ CREATE TABLE employee (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
     fk_role INT NOT NULL,
-    fk_contact INT NOT NULL,
+    fk_contact INT NOT NULL UNIQUE,
     fk_company INT NOT NULL,
     CONSTRAINT fk_opt_role FOREIGN KEY (fk_role)
     REFERENCES opt_role (id_opt_role),
     CONSTRAINT fk_cont_contact FOREIGN KEY (fk_contact)
     REFERENCES contact (id_contact),
     CONSTRAINT fk_comp_company FOREIGN KEY (fk_company)
-    REFERENCES company (id_company));
+    REFERENCES company (id_company)
+);
     
 CREATE TABLE server (
 	id_server INT PRIMARY KEY AUTO_INCREMENT,
-    motherboard_id VARCHAR(45) NOT NULL,
+    motherboard_id VARCHAR(150) NOT NULL UNIQUE,
     tag_name VARCHAR(45) NOT NULL,
     type ENUM ('cloud', 'on-premise') NOT NULL,
-    instance_id VARCHAR(45),
-    active TINYINT(1) NOT NULL,
-    so ENUM ('windows', 'linux', 'mcos') NOT NULL,
-    fk_city INT NOT NULL,
+    instance_id VARCHAR(45) UNIQUE,
+    active TINYINT(1) NOT NULL DEFAULT 1,
+    so ENUM ('windows', 'linux', 'macos') NOT NULL,
+    city VARCHAR(60) NOT NULL,
     fk_company INT NOT NULL,
     fk_country INT NOT NULL,
     CONSTRAINT fk_compa_company FOREIGN KEY (fk_company)
     REFERENCES company (id_company),
     CONSTRAINT fk_count_country FOREIGN KEY (fk_country)
-    REFERENCES country (id_country));
+    REFERENCES country (id_country)
+);
     
 CREATE TABLE component (
 	id_component INT PRIMARY KEY AUTO_INCREMENT,
     tag_name VARCHAR(45) NOT NULL,
-    type VARCHAR(45) NOT NULL,
+    type ENUM("cpu", "ram", "hd", "ssd", "swap") NOT NULL,
+    active TINYINT(1) NOT NULL DEFAULT 1,
     fk_server INT NOT NULL,
     CONSTRAINT fk_ser_server FOREIGN KEY (fk_server)
-    REFERENCES server (id_server));
+    REFERENCES server (id_server)
+);
 
 CREATE TABLE metric (
 	id_metric INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(10) NOT NULL,
-    metric VARCHAR(10) NOT NULL,
+    metric ENUM("GB", "MB", "KB", "Kbps", "Mbps", "%", "Mhz", "Ghz") NOT NULL,
     max_limit INT NOT NULL,
-    min_limit INT NOT NULL,
+    min_limit INT,
     total INT NOT NULL,
     fk_component INT NOT NULL,
     CONSTRAINT fk_com_component FOREIGN KEY (fk_component)
-    REFERENCES component (id_component));
+    REFERENCES component (id_component)
+);
 
 CREATE TABLE alert (
 	fk_metric INT NOT NULL,
@@ -95,7 +105,8 @@ CREATE TABLE register (
     current VARCHAR(45) NOT NULL,
     date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     CONSTRAINT fk_reg_metric FOREIGN KEY (fk_metric)
-    REFERENCES metric (id_metric));
+    REFERENCES metric (id_metric)
+);
 
 CREATE TABLE connection (
 	id_connection INT PRIMARY KEY AUTO_INCREMENT,
@@ -104,7 +115,8 @@ CREATE TABLE connection (
     date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     fk_server INT,
     CONSTRAINT fk_se_server FOREIGN KEY (fk_server)
-    REFERENCES server (id_server));
+    REFERENCES server (id_server)
+);
 
 INSERT INTO country (name, country_code, mask_company_registration_number, mask_phone) VALUES
 ('Brazil', 'BR', '00.000.000/0000-00', '{+55} (00) 00000-0000'),
@@ -112,6 +124,23 @@ INSERT INTO country (name, country_code, mask_company_registration_number, mask_
 ('United States', 'US', '00-00-0000000', '{+1} (000) 000-0000'),
 ('China', 'CN', '000-000000000', '{+86} (000) 0000-0000'),
 ('France', 'FR', '00 000 000 000', '{+33} 00 00 00 00');
+
+
+INSERT INTO contact (email, phone) VALUES 
+('bryangomesrocha@gmail.com', '11960976085'),
+('support@ubsoft.com', '11960971845');
+
+INSERT INTO company (commercial_name, legal_name, registration_number, fk_contact, fk_country) VALUES
+('Ubsoft', 'Ubsoft Tecnologia LTDA', '00000000000000', 2, 1);
+
+INSERT INTO opt_role (name, description) VALUES
+('manager', "Full access to the entire company profile."),
+('suport', 'Real-time Dashboard Access'),
+('Analyst', 'Access to Analytics Dashboard');
+
+INSERT INTO employee (name, gender, fk_company, fk_contact, fk_role, password) VALUES 
+('Bryan Rocha', 'Male', 1, 1, 1, '@Teste123');
+
 
 -- INSERT INTO contact (email, phone, created_at, updated_at) VALUES
 -- ('john.doe@example.com', '+12025550123', '2025-01-01 08:30:00', '2025-04-03 09:00:00'),
